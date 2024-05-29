7b749fcef7a2aa4846e5fd78974069d2694f5a22
thalytalima211 <thalytalima211@gmail.com>
2024-05-29 16:22:46 -0300

    Adiciona reservas para quartos

--- spec/system/inn_owner/inn_owner_register_group_reservation_spec.rb
+++ spec/system/inn_owner/inn_owner_register_group_reservation_spec.rb
@@ -33,8 +33,25 @@ describe 'Dono de pousada registra reserva para grupos' do
 
     expect(page).to have_content 'Reserva cadastrada com sucesso'
     expect(page).to have_content 'Reserva para time do Flamengo'
-    expect(page).to have_content "Data de Início: #{1.week.from_now}"
-    expect(page).to have_content "Data de Término: #{3.weeks.from_now}"
-    expect(page).to have_content 'Não há nenhum quarto cadastrado para essa reserva'
+    expect(page).to have_content "Data de Início: #{1.week.from_now.to_date}"
+    expect(page).to have_content "Data de Término: #{3.weeks.from_now.to_date}"
+  end
+
+  it 'e vê reservas' do
+    inn_owner = InnOwner.create!(first_name: 'Joao', last_name: 'Almeida',
+                                  document: '53783222001', email: 'joao@email.com', password: '123456')
+    inn = inn_owner.create_inn!(name: 'Pousada do Almeidinha', registration_number: '30638898000199',
+                                description: 'Um bom lugar', address_attributes: { address: 'Rua X', number: '100',
+                                city: 'Manaus', state: 'AM', postal_code: '69067-080', neighborhood: 'Centro'})
+    reservation = Reservation.create!(name: 'Reserva para o time do Flamengo', start_date: 1.week.from_now,
+                                      end_date: 3.weeks.from_now, inn: inn)
+
+    login_as inn_owner, scope: :inn_owner
+    visit root_path
+    click_on 'Gestão de Pousada'
+
+    within('#reservation') do
+      expect(page).to have_content 'Reserva para o time do Flamengo'
+    end
   end
 end
